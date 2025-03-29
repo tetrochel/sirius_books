454ec43609a43ee31600852c2a1c31cf18d5220a
Max <hpackarda433@gmail.com>
2025-03-29 17:45:12 +0300

    Presentation перемещён в packages

--- presentation/lib/presentation.dart
+++
@@ -1,4 +0,0 @@
-export 'package:presentation/theme/theme_picker.dart';
-export 'package:presentation/widgets/book_widget.dart';
-export 'package:presentation/widgets/auth_text_field_widget.dart';
-export 'package:presentation/widgets/navbar_widget.dart';
--- presentation/lib/theme/theme_picker.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-part 'text_styles.dart';
-part 'app_colors.dart';
-
-extension ThemePicker on BuildContext {
-  AppColors get colors => Theme.of(this).extension<AppColors>()!;
-  AppTextStyles get textStyles => Theme.of(this).extension<AppTextStyles>()!;
-}
