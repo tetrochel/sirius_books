789200d7e0b62b9f3f763dda4bfeb8557787377e
Max <hpackarda433@gmail.com>
2025-03-31 17:06:49 +0300

    Добавлен виджет выставки и экран с выставками

---
+++ lib/features/exposition/ui/pages/expositions_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:presentation/presentation.dart';
+import 'package:sirius_books/features/exposition/data/model/exposition_model.dart';
+import 'package:sirius_books/generated/app_localizations.dart';
+
+class ExpositionsPage extends StatelessWidget {
+  late final List<int> subscriptions = List.generate(
+    4,
+    (index) => index * 2,
+  );
+  late final List<ExpositionModel> expositions = List.generate(
+    10,
+    (index) => ExpositionModel(
+      id: index,
+      name: 'Русская классика',
+      topic: '',
+      location: '',
+      bookList: [],
+      startDate: DateTime(2025, 2, 1),
+      endDate: DateTime(2025, 2, 5),
+    ),
+  );
+
+  ExpositionsPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          shadowColor: Colors.black,
+          backgroundColor: context.colors.white,
+          surfaceTintColor: context.colors.white,
+          title: AppBarWidget(
+            title: AppLocalizations.of(context)!.expositions,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  color: context.colors.primary,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  // TODO(ivan): Добавить вызов модального окна через router
+                },
+                icon: Icon(
+                  Icons.filter_alt,
+                  color: context.colors.primary,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          sliver: SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              mainAxisSpacing: 24,
+              childAspectRatio: 1.5,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => ExpositionWidget(
+                name: expositions[index].name,
+                onTap: () {},
+                onTapSubscribe: () {},
+                isSubscribed: subscriptions.contains(index),
+                startDate: DateFormat('dd.MM.yyyy')
+                    .format(expositions[index].startDate),
+                endDate:
+                    DateFormat('dd.MM.yyyy').format(expositions[index].endDate),
+              ),
+              childCount: expositions.length,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/user/ui/pages/auth_page.dart
+++ lib/features/user/ui/pages/auth_page.dart
@@ -9,34 +9,36 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: AuthTextField(
-                controller: TextEditingController(),
-                focusNode: FocusNode(),
-                hint: AppLocalizations.of(context)!.email,
-                label: AppLocalizations.of(context)!.email,
-                isPassword: false,
-                prefixIcon: Icons.person,
-                obscureText: false,
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AuthTextField(
+                  controller: TextEditingController(),
+                  focusNode: FocusNode(),
+                  hint: AppLocalizations.of(context)!.email,
+                  label: AppLocalizations.of(context)!.email,
+                  isPassword: false,
+                  prefixIcon: Icons.person,
+                  obscureText: false,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: AuthTextField(
-                controller: TextEditingController(),
-                focusNode: FocusNode(),
-                hint: AppLocalizations.of(context)!.password,
-                label: AppLocalizations.of(context)!.password,
-                isPassword: true,
-                prefixIcon: Icons.key,
-                obscureText: true,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AuthTextField(
+                  controller: TextEditingController(),
+                  focusNode: FocusNode(),
+                  hint: AppLocalizations.of(context)!.password,
+                  label: AppLocalizations.of(context)!.password,
+                  isPassword: true,
+                  prefixIcon: Icons.key,
+                  obscureText: true,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
