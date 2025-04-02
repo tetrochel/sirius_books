b9e31063a781a4dfbcdca13a2668cc5d17f1e67c
Ranot27 <rannot27@gmail.com>
2025-04-02 11:48:30 +0300

    Добавил загрузки книг с firebase, добавил Logout пользователя

--- lib/config/navigation/navigation.dart
+++ lib/config/navigation/navigation.dart
@@ -59,7 +59,7 @@ final GoRouter router = GoRouter(
           path: '/books',
           pageBuilder: (context, state) => noTransitionPage(
             state: state,
-            child: BooksPage(),
+            child: const BooksPage(),
           ),
           routes: [
             GoRoute(
