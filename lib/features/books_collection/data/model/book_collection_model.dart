483463e56dc4454efd5064efe0a5ef7d8b802492
Ranot27 <rannot27@gmail.com>
2025-04-03 21:14:14 +0300

    Сделано добавление колеекции

--- lib/features/books_collection/data/model/book_collection_model.dart
+++ lib/features/books_collection/data/model/book_collection_model.dart
@@ -1,15 +1,15 @@
 import 'package:sirius_books/features/book/data/model/book_model.dart';
 
 class CollectionModel {
-  final String firebaseId;
+  final String? firebaseId;
   final String name;
   final String description;
   final List<BookModel> books;
 
   CollectionModel({
-    required this.firebaseId,
     required this.name,
     required this.description,
     required this.books,
+    this.firebaseId,
   });
 }
