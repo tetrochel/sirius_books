0c4aee9ea45e2460fa02fcc11b18e2fb020bb782
tetrochel <ivaan122s@gmail.com>
2025-04-03 11:07:37 +0300

    Перенесен FloatingActionButton в общий скаффолд

--- lib/config/navigation/navigation.dart
+++ lib/config/navigation/navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:presentation/presentation.dart';
 import 'package:sirius_books/features/book/ui/pages/books_page.dart';
@@ -6,6 +7,8 @@ import 'package:sirius_books/features/books_collection/ui/pages/books_collection
 import 'package:sirius_books/features/exposition/ui/pages/expositions_page.dart';
 import 'package:sirius_books/features/exposition/ui/pages/new_exposition_page.dart';
 import 'package:sirius_books/features/filter/ui/pages/filter_page.dart';
+import 'package:sirius_books/features/user/data/model/user_model.dart';
+import 'package:sirius_books/features/user/ui/bloc/user_bloc.dart';
 import 'package:sirius_books/features/user/ui/pages/auth_page.dart';
 import 'package:sirius_books/features/user/ui/pages/registration_page.dart';
 import 'package:sirius_books/features/user/ui/pages/reset_password_page.dart';
@@ -15,34 +18,62 @@ final GoRouter router = GoRouter(
   initialLocation: '/expositions',
   routes: [
     ShellRoute(
-      builder: (context, state, child) => Scaffold(
-        body: SafeArea(
-          child: child,
-        ),
-        bottomNavigationBar: NavBar(
-          items: [
-            (
-              label: AppLocalizations.of(context)!.expositions,
-              icon: Icons.calendar_today,
-            ),
-            (
-              label: AppLocalizations.of(context)!.books,
-              icon: Icons.menu_book,
-            ),
-            (
-              label: AppLocalizations.of(context)!.collections,
-              icon: Icons.collections_bookmark,
-            ),
-          ],
-          currentIndex: _getCurrentIndex(state),
-          onTabSelected: (index) => _switchPage(context, index),
-        ),
-      ),
+      builder: (context, state, child) {
+        var showActionButton = false;
+        if (context.watch<UserBloc>().state.userModel != null &&
+            context.watch<UserBloc>().state.userModel!.role == UserRole.keeper) {
+          showActionButton = true;
+        }
+        if (state.fullPath == '/collections') {
+          showActionButton = false;
+        }
+        return Scaffold(
+          body: SafeArea(
+            child: child,
+          ),
+          floatingActionButton: showActionButton
+              ? FloatingActionButton(
+                  onPressed: () {
+                    if (state.fullPath == '/expositions') {
+                      context.go('/expositions/new');
+                    } else {
+                      context.go('/books/new');
+                    }
+                  },
+                  child: const Icon(Icons.add),
+                  backgroundColor: context.colors.primary,
+                  foregroundColor: context.colors.white,
+                )
+              : null,
+          bottomNavigationBar: NavBar(
+            items: [
+              (
+                label: AppLocalizations.of(context)!.expositions,
+                icon: Icons.calendar_today,
+              ),
+              (
+                label: AppLocalizations.of(context)!.books,
+                icon: Icons.menu_book,
+              ),
+              (
+                label: AppLocalizations.of(context)!.collections,
+                icon: Icons.collections_bookmark,
+              ),
+            ],
+            currentIndex: _getCurrentIndex(state),
+            onTabSelected: (index) => _switchPage(context, index),
+          ),
+        );
+      },
       routes: [
         GoRoute(
           path: '/expositions',
           pageBuilder: (context, state) => noTransitionPage(
-            child: ExpositionsPage(),
+            // ColoredBox для фона
+            child: ColoredBox(
+              color: context.colors.white,
+              child: ExpositionsPage(),
+            ),
             state: state,
           ),
           routes: [
@@ -127,8 +158,7 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: '/filter',
-      pageBuilder: (context, state) =>
-          const ModalPage<void>(child: FilterPage()),
+      pageBuilder: (context, state) => const ModalPage<void>(child: FilterPage()),
     ),
   ],
 );
