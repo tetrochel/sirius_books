8904f0e9c2563958cea32214c1b5fee8e8f730e7
tetrochel <ivaan122s@gmail.com>
2025-04-04 08:53:49 +0300

    пофикисил экран выставок

--- lib/features/exposition/ui/pages/exposition_details_page.dart
+++ lib/features/exposition/ui/pages/exposition_details_page.dart
@@ -6,6 +6,8 @@ import 'package:sirius_books/config/constants.dart';
 import 'package:sirius_books/features/exposition/data/model/exposition_model.dart';
 import 'package:sirius_books/features/exposition/ui/bloc/exposition_bloc.dart';
 import 'package:sirius_books/features/exposition/ui/bloc/exposition_event.dart';
+import 'package:sirius_books/features/user/data/model/user_model.dart';
+import 'package:sirius_books/features/user/ui/bloc/user_bloc.dart';
 import 'package:sirius_books/features/utils/enums.dart';
 import 'package:sirius_books/generated/app_localizations.dart';
 
@@ -53,6 +55,17 @@ class _ExpositionDetailsPageState extends State<ExpositionDetailsPage> {
   }
 
   @override
+  void dispose() {
+    nameController.dispose();
+    topicController.dispose();
+    descriptionController.dispose();
+    locationController.dispose();
+    startDateController.dispose();
+    endDateController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final readOnly = mode == Mode.view;
     return Scaffold(
@@ -61,7 +74,6 @@ class _ExpositionDetailsPageState extends State<ExpositionDetailsPage> {
           physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           slivers: [
             SliverAppBar(
-              titleSpacing: 0,
               pinned: true,
               shadowColor: Colors.black,
               backgroundColor: context.colors.white,
@@ -69,44 +81,47 @@ class _ExpositionDetailsPageState extends State<ExpositionDetailsPage> {
               title: AppBarWidget(
                 title: widget.exposition.name,
                 disablePadding: true,
-                actions: [
-                  if (mode == Mode.view)
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          mode = Mode.edit;
-                        });
-                      },
-                      icon: Icon(
-                        Icons.edit,
-                        color: context.colors.primary,
-                      ),
-                    )
-                  else
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          mode = Mode.view;
-                        });
-                        context.read<ExpositionBloc>().add(
-                              OnUpdateExpositionPressed(
-                                expositionModel: exposition.copyWith(
-                                  name: nameController.text,
-                                  topic: topicController.text,
-                                  description: descriptionController.text,
-                                  location: locationController.text,
-                                  startDate: startDate ?? exposition.startDate,
-                                  endDate: endDate ?? exposition.endDate,
-                                ),
-                              ),
-                            );
-                      },
-                      icon: Icon(
-                        Icons.check,
-                        color: context.colors.primary,
-                      ),
-                    ),
-                ],
+                actions: context.watch<UserBloc>().state.userModel != null &&
+                        context.watch<UserBloc>().state.userModel!.role == UserRole.keeper
+                    ? [
+                        if (mode == Mode.view)
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                mode = Mode.edit;
+                              });
+                            },
+                            icon: Icon(
+                              Icons.edit,
+                              color: context.colors.primary,
+                            ),
+                          )
+                        else
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                mode = Mode.view;
+                              });
+                              context.read<ExpositionBloc>().add(
+                                    OnUpdateExpositionPressed(
+                                      expositionModel: exposition.copyWith(
+                                        name: nameController.text,
+                                        topic: topicController.text,
+                                        description: descriptionController.text,
+                                        location: locationController.text,
+                                        startDate: startDate ?? exposition.startDate,
+                                        endDate: endDate ?? exposition.endDate,
+                                      ),
+                                    ),
+                                  );
+                            },
+                            icon: Icon(
+                              Icons.check,
+                              color: context.colors.primary,
+                            ),
+                          ),
+                      ]
+                    : [],
               ),
             ),
             SliverPadding(
@@ -135,9 +150,7 @@ class _ExpositionDetailsPageState extends State<ExpositionDetailsPage> {
                     readOnly: readOnly,
                   ),
                   DateFieldWidget(
-                    onTap: mode == Mode.edit
-                        ? () => _selectDate(context, false)
-                        : null,
+                    onTap: mode == Mode.edit ? () => _selectDate(context, false) : null,
                     controller: startDateController,
                     label: AppLocalizations.of(context)!.startDate,
                   ),
@@ -145,9 +158,7 @@ class _ExpositionDetailsPageState extends State<ExpositionDetailsPage> {
                     height: 14,
                   ),
                   DateFieldWidget(
-                    onTap: mode == Mode.edit
-                        ? () => _selectDate(context, true)
-                        : null,
+                    onTap: mode == Mode.edit ? () => _selectDate(context, true) : null,
                     controller: endDateController,
                     label: AppLocalizations.of(context)!.endDate,
                   ),
